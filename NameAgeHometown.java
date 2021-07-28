d58505c9a647aa9de27c8ce8dcd596ba9839d2bf
davdbohang <78942759+davdbohang@users.noreply.github.com>
2021-07-28 12:52:01 -0700

    Add files via upload

---
+++ NameAgeHometown.java
@@ -0,0 +1,7 @@
+public class NameAgeHometown {
+    public static void main(String[] args) {
+        System.out.println("My name is David");
+        System.out.println("I am 34 years old");
+        System.out.println("My hometown is Hayward, CA");
+    }
+}
