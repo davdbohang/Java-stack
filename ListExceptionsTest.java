a681f60dd1c2c710ee63818c7e63c78a4f9195b5
davdbohang <78942759+davdbohang@users.noreply.github.com>
2021-07-31 14:49:24 -0700

    Add files via upload

---
+++ ListExceptionsTest.java
@@ -0,0 +1,8 @@
+import java.util.ArrayList;
+
+public class ListExceptionsTest {
+    public static void main(String[] args) {
+        ListExceptions test = new ListExceptions();
+        test.castToInt();
+    }
+}
