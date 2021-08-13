b3d9083ed66690565d3c6597c5de44c6f0cba692
davdbohang <78942759+davdbohang@users.noreply.github.com>
2021-08-13 09:34:43 -0700

    Add files via upload

---
+++ HellohumanApplication.java
@@ -0,0 +1,29 @@
+package com.dhang.hellohuman;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@SpringBootApplication
+@RestController
+public class HellohumanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HellohumanApplication.class, args);
+	}
+	
+	@RequestMapping("/")
+	public String index(@RequestParam(value="name", required=false) String name) {
+		if (name == null){
+			return "Hello Human!";
+		} else {
+			return "Hello "  + name + "!";
+		}
+	}
+
+}
